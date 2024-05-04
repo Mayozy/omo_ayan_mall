1dbbb5f9164b06795cba4921fd21707acbc5d4cf
Samuel Ayangbile <165306899+Mayozy@users.noreply.github.com>
2024-05-04 10:15:49 -0700

    Add files via upload

---
+++ omo_ayan_mall.py
@@ -0,0 +1,19 @@
+#a program that takes a list of certain items
+'''
+item_id
+item_name
+item_count
+item_price
+item_purchased_date
+'''
+
+my_dict = {
+    "item_id" :"block" ,
+    "number_of_items" : "73" ,
+    "purchased_date" : [(input("date of item purchased : "))]}
+
+print(my_dict["item_id"])
+print(my_dict["number_of_items"])
+print(my_dict["purchased_date"])
+
+              
